2206a2189dcccf3cded3d3ccf41e493437d6f38f
sgeorgiou1 <83709427+sgeorgiou1@users.noreply.github.com>
2024-05-15 12:12:10 +0100

    Update suspect_list_generator.py
    Fix info error, add function that saves term statistics as a csv also

--- suspect_list_generator.py
+++ suspect_list_generator.py
@@ -17,10 +17,10 @@ from bypass_checker import bypass_checker
 import os
 
 # Specify the path where you want to create the folder
-def setup_directory(folder_path):
+def setup_directory(folder_path, folder_name):
 
     # Create the directory
-    os.makedirs(folder_path +"indexing_docs", exist_ok=True)
+    os.makedirs(folder_path + folder_name, exist_ok=True)
     
 def get_prefix(indtype): #this is just for the file name prefix depending on the indexing type
     if indtype == "INSPEC Thesaurus":
@@ -61,7 +61,7 @@ def ind_extract(path, indtype): #this uses the dodgyextract function to extract 
             entry['Precision'] = entry['Correct'] / entry['CESTotal']
         else:
             entry['Precision'] = "NaN"
-            
+    
     #use csv_writer to save indexing output
     csv_writer(path,lines,indtype)
     
@@ -71,13 +71,19 @@ def ind_extract(path, indtype): #this uses the dodgyextract function to extract 
     #generate file prefix
     prefix = get_prefix(indtype)
     
+    #convert dict to dataframe
+    term_df = pd.DataFrame.from_dict(term_dict,orient="index")
+    final_term_df = term_df.reset_index().rename(columns={"index":"Term"}) #reset index column to the terms
+    
+    final_term_df.to_csv(path+f'indexing_docs\\{prefix}stats_table.csv') # also save a datatable for easier analysis
+    
     #write JSON file
-    with open(path + f'indexing_docs\\{prefix}stats.json', 'w', encoding= 'utf-8') as f:
+    with open(path + f'indexing_docs\\{prefix}stats_dict.json', 'w', encoding= 'utf-8') as f:
         f.write(json_dict)
     
     return lines, term_dict
 
-def info():
+def print_info():
     notice = """
     ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     This is the suspect_list_generator code @author: Stellios
@@ -130,13 +136,14 @@ def info():
     
     .save_lists() - saves the suspect lists
 
-    type suspect_list_generator.info() to get this info again
+    .info() to get this info again
     ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     """
     print (notice)
 def suspect_terms_generator(indtype, ind_df, ind_stats, bypass_cutoff, max_precision = 0, desired_bypass = 0):
 
     #first cut down dataframe to the relevant columns, we only need ID and CES indexing    
+    prefix = get_prefix(indtype)
     indexing = ind_df[["ID","CES"]]
     
     
@@ -165,23 +172,22 @@ def suspect_terms_generator(indtype, ind_df, ind_stats, bypass_cutoff, max_preci
         print (f'{(len(ind_dict)-bypass_count)} sent to QA')
         return sus_list, ind_dict, bypass_pctage
         
-    
     if desired_bypass != 0: # if we are creating a suspect list based on a desired bypass value this part of the function will run
         precision_values = []
         bypass_pctage = 0
-        prec_range = np.arange(0.05,0.85,0.05)[::-1] # create a basic range of precision values to test
+        prec_range = np.arange(0.05,1,0.05)[::-1] # create a basic range of precision values to test
         prec_range = np.round(prec_range,2) # this negates any float issues from numpy
         #print (prec_range)
         for prec in prec_range: # loop through precision values in the range
             bypass_count = 0
             print ("="*80)
-            print (f"Trying suspect list with term precision: {prec}")
+            print (f"Trying {prefix} suspect list with term precision <= {prec}")
             sus_list = []
             for term in ind_stats: 
                 entry = ind_stats[term] 
                 if  isinstance(entry['Precision'],float) and entry['Precision'] <= prec and entry['CESTotal'] >= 10: # creating a suspect list based on current precision value in the range
                     sus_list.append(term)
-            print ("Suspect list lenght = ", len(sus_list))
+            print ("Suspect list length = ", len(sus_list))
             for item in ind_dict: # test current suspect list against items in dictionary
                 indexing = ind_dict[item]["indexing"]
                 output, susterms = bypass_checker(sus_list,indexing,bypass_cutoff)
@@ -194,11 +200,12 @@ def suspect_terms_generator(indtype, ind_df, ind_stats, bypass_cutoff, max_preci
             print (f'{(len(ind_dict)-bypass_count)} sent to QA')
                         
             precision_values.append((prec, bypass_pctage)) # append test values to a list to display different variations
-            if bypass_pctage >= 0.99 * desired_bypass: # if the bypass rate is within 95% of your desired bypass then break the loop and output this configuration
+            agreement = bypass_pctage - desired_bypass 
+            if agreement <=0.05 and agreement >= -0.05:# if the bypass rate is within 95% of your desired bypass then break the loop and output this configuration
                 break
             
         print ("="*80)
-        print (f'Ideal precision cutoff is {prec} with a bypass rate of {bypass_pctage}')
+        print (f'Ideal {prefix} precision cutoff is {prec} with a bypass rate of {bypass_pctage}, list length: {len(sus_list)}')
         print ('Other values: ', precision_values)
         return sus_list, ind_dict, bypass_pctage
     
@@ -210,8 +217,8 @@ class suspect_list_generator:
         self.extract = extract
         self.ct_indtype = "INSPEC Thesaurus"
         self.cc_indtype = "INSPEC Classification"
-        
-        setup_directory(path) # sets up output directory
+        print_info()
+        setup_directory(path, "indexing_docs") # sets up output directory
         
         if extract == True: # run extract codes if extraction argument is triggered
 
@@ -231,7 +238,9 @@ class suspect_list_generator:
                 
         self.ct_indexing = pd.read_csv(path + "indexing_docs/CT_indexing.csv")
         self.cc_indexing = pd.read_csv(path + "indexing_docs/CC_indexing.csv")
-        
+    
+    def info():
+        print_info()
     def generate_ct_list(self, bypass_cutoff, max_precision = 0, desired_bypass = 0): # this runs the suspect terms generator function with your given values
         if max_precision == 0 and desired_bypass == 0:
             raise ValueError("Please assign either a max_precision or desired_bypass to create suspect terms list")
@@ -253,6 +262,7 @@ class suspect_list_generator:
         #return self.cc_suspect_list, self.cc_ind_dict, self.cc_bypass_pcsage
     
     def save_lists(self):
+        setup_directory(self.path, "suspect_lists")
         try:
             cts_df = pd.DataFrame.from_dict(self.ct_ind_dict, orient = "index")
             cts_df = cts_df.reset_index(names = "ID")
@@ -261,13 +271,11 @@ class suspect_list_generator:
             ccs_df = ccs_df.reset_index(names = "ID")
             
             
-            cts_df.to_csv(self.path + "indexing_docs/CT_suspect_output.csv")
-            ccs_df.to_csv(self.path + "indexing_docs/CC_suspect_output.csv")    
+            cts_df.to_csv(self.path + "suspect_lists/CT_suspect_output.csv")
+            ccs_df.to_csv(self.path + "suspect_lists/CC_suspect_output.csv")    
         
-            pd.DataFrame(self.ct_suspect_list, columns = ["Term"]).to_csv((self.path + "indexing_docs/CT_suspect_list.csv"))
-            pd.DataFrame(self.cc_suspect_list, columns = ["Term"]).to_csv((self.path + "indexing_docs/CC_suspect_list.csv"))
+            pd.DataFrame(self.ct_suspect_list, columns = ["Term"]).to_csv((self.path + "suspect_lists/CT_suspect_list.csv"))
+            pd.DataFrame(self.cc_suspect_list, columns = ["Term"]).to_csv((self.path + "suspect_lists/CC_suspect_list.csv"))
         
         except:
             raise ValueError("Suspect lists must be generated before you can save them")
-
-              
